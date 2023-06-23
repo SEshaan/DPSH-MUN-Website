774f66375428c96342b0d0f6ffb553d18873f8dc
YajTPG <yajtpg@gmail.com>
2023-06-23 16:57:51 +0530

    Optimize images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		ViteImageOptimizer({
+			// include: '*/static/SC/*.JPG'
+			jpg: {
+				quality: 30
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
