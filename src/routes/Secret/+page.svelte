195ba0e4e05d000e41f53595248677feefa61639
greatesh <70007115+greatesh@users.noreply.github.com>
2023-07-07 19:04:00 +0530

    hmm

--- src/routes/Secret/+page.svelte
+++ src/routes/Secret/+page.svelte
@@ -1,6 +1,8 @@
 
 
 <div id="directive">
+    <h1>Highlights from 2022</h1> <br> <br>
+
     <h1>Directive Oil Stonks Global Domination</h1>
     COVERT,<br>
     COUNTRIES - THE RUSSIAN FEDERATION, HUNGARY, ARGENTINA,<br>
@@ -12,6 +14,127 @@
     Storm the Saudi nuclear arsenal and take over physical access to the nuclear weapons and cut off the ISIS from their nuclear weaponry,
     A full on Russia-Hungary joint military operation  neutralising ISIS terrorists on sight and capturing and war prisoners(if any),
     A take over of the Saudi capital after which a government, under the control of the Tripartite coalition of Russia, Hungary and Argentina will be established to prevent such a crisis from rising again.
+
+    <h1>Directive Master Chief</h1>
+<pre>
+    Covert
+Country: United Arab Emirates, Japan, Canada, Germany, Australia, France, Switzerland,
+Russian Federation, People’s Republic Of China, Republic of India
+Primary Objective: Denuclearization of the terror organization ISIS
+Secondary Objective: Neutralization of launch sites in possession of ISIS
+Plan of Action:
+
+1. On 30th September 2022, The Federal Security Service of the Russian
+Federation (FSB), Canadian Canadian Security Intelligence Service (CSIS), the
+Indian Research and Analysis Wing (R&AW), the French Directorate-General for
+External Security, the Chinese Ministry of State Security, and the Swiss Federal
+Intelligence Service (FIS) will use ground assets to perform reconnaissance and
+identify
+a. Facilities controlled by ISIS that are capable of producing and storing
+Nuclear Weapons of Mass Destruction and Radioactive materials in Saudi
+Arabia.
+b. Launch sites and facilities controlled by ISIS capable of launching Nuclear
+Weapons of Mass Destruction in Saudi Arabia
+c. Landing Zones for aircraft.
+The information retrieved from this operation will be shared with all
+members of the directive.
+
+2. On, 1st September 2022, The German Luftwaffe and the United Arab Emirates
+will send 1 EMT Luna X-2000 and 1 MQ-1 Predator respectively to the sites
+scouted by the Federal Security Service of the Russian Federation (FSB), , the
+Indian Research and Analysis Wing (R&AW), Canadian Canadian Security
+Intelligence Service (CSIS), the French Directorate-General for External Security,
+the Chinese Ministry of State Security, and the Swiss Federal Intelligence Service
+(FIS) in coordination with intelligence provided by Japanese reconnaissance
+data.
+a. The MQ-1 Predator and the EMT Luna X-2000 will be taking flight from Al
+Dhafra Air Base.
+
+3. On 3rd October 2022, at 2230 hours, The Japan Air Self-Defence Force and
+United Arab Emirates Air Force will send F-15 Eagles and F-16 Falcons to
+a. Provide Air Cover to ground troops .
+b. Perform Aerial Bombardment of Launch Sites scouted .
+4. On 4th October 2022, at 0130 hours, 10 UH-60 Black Hawk Helicopters and 10
+CH-47 Chinook Helicopters carrying 200 special forces operatives from United
+Arab Emirates Presidential Guard, Kommando Spezialkräfte, Canadian Special
+Operations Forces Command, Special forces of Australia will breach the facilities
+scouted to retrieve radioactive sources and disarm and destroy nuclear launch
+facilities internally. The commandos will be transported using MRZR-D and
+HMMWV utility vehicles.
+
+a. UAE Presidential Guard Commandos will be armed with SIG Sauer P320
+Handguns and CAR 816/Sultan Assault Rifles.
+b. The Commandos from the Kommando Spezialkräfte will be armed with
+G36 and HK416 Assault Riles, MP5 handguns, combat knives, Flash
+Grenades, Smoke Grenades, and Tear Gas grenades.
+c. The Commandos of the Special forces of Australia will be armed with
+AMR 50 Cal. Sniper Rifles, KH416 Assault Rifles, F89 Machine Gun, and
+MP5 Submachine guns.
+d. The Commandos of the Canadian Special Operations Forces Command
+will be armed with Heckler & Koch MP5 (Joint Task Force 2), FN Herstal
+P90, Armalite AR-10(T), BGM-71 TOW, Heckler & Koch GMG, Spike, and
+Carl Gustaf 8.4 cm recoilless Rifle.
+
+5. Recovered radioactive material will be retrieved from scouted landing zones
+using 8 ARV3M Armoured Recovery Vehicle.
+a. The Armoured Recovery Vehicle will be brought to the landing zones and
+retrieved from the same along with the special forces operatives from the
+countries as mentioned earlier using 10 UH-60 Black Hawk Helicopters
+and 10 CH-47 Chinook Helicopters as specified earlier.
+
+6. The aircrafts mentioned above will operate from Al Dhafra and Al Minhad Air
+Bases.
+7. The recovered radioactive material will be temporarily stored at the Al Barakah
+Nuclear Power Plant in Abu Dhabi in the United Arab Emirates.
+
+</pre>
+
+
+<h1>
+    Operation USA go boom
+</h1>
+
+<pre>
+    
+Primary Objective:- detonating the nuclear weapons in USA
+
+Secondary objective:- complete destruction of the USA
+
+ 
+
+Plan of Action:-
+
+1.       All sleeper agents and terrorist organizations in USA to detonate the dirty bombs in their possession in major US cities
+
+2.       Arm all terrorist organizations and march to Washington and declare a worker’s revolution
+
+3.       Commit the assassination of Jeff Bezos, Elon Musk and other billionaires in the name of the revolution
+
+</pre>
+
+<h1>
+    Directive Endgame
+</h1>
+
+<pre>
+    Covert
+Argentina
+
+Primary Objective: Establish Argentine control over Iraq
+Secondary Objective: Secure Argentine’s position in the post crisis economic recession
+
+Plan of action:
+A) Gaining Access to Iraq via
+1. The Puppet Government to be reinstated in Saudi Arabia (Directive oil stonks world
+domination)
+2. Alliance with Turkey, Iran and Israel (directive topside, directive right side, directive 3-
+side)
+B) Oil market interference
+1. Crushing world economy by inflated oil prices (Directive Oil stonks world
+domination)
+2. Getting royalties on oil sales via puppet government in Saudi Arabia (Directive oil
+stonks world domination)
+</pre>
 </div>
 
 <style>
@@ -22,6 +145,13 @@
         width: 50vw;
         margin: auto;
     }
+    #directive pre {
+    white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+}
     @media (max-width:600px){
         #directive{
             width: 80vw;
